25fdd0c0020162f412f2de2eb3fcf056de145a40
SantiagoAlvarze22 <santiago.alvarez@est.iudigital.edu.co>
2026-07-31 16:47:18 -0500

    AccessDeniedException permisos aws

--- dags/nyc_tlc_dag.py
+++ dags/nyc_tlc_dag.py
@@ -3,6 +3,10 @@ from airflow.sdk import DAG, task
 from time import sleep
 import boto3 
 from botocore.exceptions import ClientError
+import logging
+
+# Configurar el logger estándar de Airflow
+logger = logging.getLogger("airflow.task")
 
 def crawler_automatico():
     try:
@@ -14,9 +18,7 @@ def crawler_automatico():
             #si está listo para que lo llamen, entonces que inicie el crawler
             if crawler_state == 'READY':
                 #Inicia el crawler
-                glue_client.start_crawler(
-                                        Name='silver-crawler-2'
-                                        )
+                glue_client.start_crawler(Name='silver-crawler-2')
             else:
                 print(f"El crawler ya se está ejecutando. Estado actual: {crawler_state}")
 
@@ -33,7 +35,14 @@ def crawler_automatico():
                 elif crawler_state == 'RUNNING' or crawler_state == 'STOPPING':
                      sleep(30)
                      continue
-                
+    #Toma la AccessDeniedException si no hay permisos para el usuario desde aws 
+    except glue_client.exceptions.AccessDeniedException as e:
+        print(f"Access Denied (Specific): {e.response['Error']['Message']}")
+        logger.error(f'Fallo en la operación: {e}', exc_info=True)
+        
+        #Para que la tarea se lance como error y marque FAILED    
+        raise e
+            
     except ClientError as err:
             if err.response['Error']['Code'] == 'EntityNotFoundException':
                 print("Error: The specified crawler does not exist.")
