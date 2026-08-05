d3d128d0cedc4e5a6a50123a456908aef47f1331
SantiagoAlvarze22 <santiago.alvarez@est.iudigital.edu.co>
2026-08-04 21:02:59 -0500

    consultas y capa gold con tablas creadas en athena

--- sql/consultas/ingreso_promedio_por_zona.sql
+++ sql/consultas/ingreso_promedio_por_zona.sql
@@ -2,5 +2,5 @@ SELECT tz.Zone, ROUND(AVG(ds.total_amount),2) as "promedio_ingreso"
 FROM datos_silver ds 
 LEFT JOIN taxi_zone_lookup tz on tz.locationid = ds.pulocationid
 GROUP BY tz.zone
-ORDER BY promedio_ingreso DESC
+ORDER BY promedio_ingreso DESC;
 
